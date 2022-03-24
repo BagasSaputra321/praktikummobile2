90c71e77de117db1e47783098ccf2e7086770a57
Naftael Bagas <101777142+BagasSaputra321@users.noreply.github.com>
2022-03-24 23:13:41 +0800

    Update PRAK103.kt

--- Modul1/PRAK103.kt
+++ Modul1/PRAK103.kt
@@ -1,10 +1,12 @@
 fun main(){
     println("Masukkan Angka: ")
     var input= readLine()!!.toInt()
-    for (i in 2..8){
+    var i = 1
+    while (i<5){
         var hasil = i * input
         if (hasil%2==0 || hasil%3==0){
             println(hasil)
         }
+        ++i
     }
 }
